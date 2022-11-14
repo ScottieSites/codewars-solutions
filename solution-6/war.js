c6df56ece00d4567bc356adafff86cba6a8fffcd
Scott Williams <ScottWilliams220608@gmail.com>
2022-11-14 17:10:11 -0500

    solution-6

---
+++ solution-6/war.js
@@ -0,0 +1,7 @@
+function noSpace(x){
+let replaced  = x.replace(/ /g, '');
+return replaced
+}
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')
+noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')
+noSpace('8aaaaa dddd r     ')
